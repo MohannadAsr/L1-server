7b55290ec7ae13cd07121c25747d302570bb9669
Mohannad Adnan Alassar <mohannad.alassar@hotmail.com>
2024-03-27 23:24:53 +0300

    try

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const multer = require('multer'); // For handling file uploads
 
 const cors = require('cors');
 const vipsRouter = require('./routes/VipsRouter');
-const productRouter = require('./routes/ProductsRouter');
+// const productRouter = require('./routes/ProductsRouter');
 const evenstRouter = require('./routes/EventsRouter');
 const usersRouter = require('./routes/usersRouter');
 const invitationsRouter = require('./routes/InvitationsRouter');
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/vips', vipsRouter);
-app.use('/api/products', productRouter);
+// app.use('/api/products', productRouter);
 app.use('/api/events', evenstRouter);
 app.use('/api/invitaions', invitationsRouter);
 app.use('/users', usersRouter);
